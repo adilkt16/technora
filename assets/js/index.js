f5ddbee1cbfe425dad9316b0606912079ee4881e
adilkt <adilkt16@gmail.com>
2024-02-12 16:33:33 +0530

    single event page created

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var eventList = document.getElementById("event-list");
-    var showEventsButton = document.getElementById("showEvents");
-    var hideEventsButton = document.getElementById("hideEvents");
+function toggleMenu() {
+    var navMenu = document.getElementById('navMenu');
+    var burgerMenu = document.querySelector('.burger-menu');
 
-    if (eventList && showEventsButton && hideEventsButton) {
-        var allEvents = eventList.querySelectorAll("li");
-        var maxVisibleEvents = 4;
+    navMenu.classList.toggle('show');
+    burgerMenu.classList.toggle('cross');
+}
 
-        // Hide all events initially
-        for (var i = maxVisibleEvents; i < allEvents.length; i++) {
-            allEvents[i].style.display = "none";
-        }
 
-        // Show more or hide events on button click
-        showEventsButton.addEventListener("click", function (event) {
-            event.preventDefault(); // Prevent the default anchor behavior
-            for (var i = maxVisibleEvents; i < allEvents.length; i++) {
-                allEvents[i].style.display = "block";
-            }
-            showEventsButton.style.display = "none";
-            hideEventsButton.style.display = "block";
-        });
+// document.addEventListener("DOMContentLoaded", function () {
+//     var eventList = document.getElementById("event-list");
+//     var showEventsButton = document.getElementById("showEvents");
+//     var hideEventsButton = document.getElementById("hideEvents");
 
-        hideEventsButton.addEventListener("click", function (event) {
-            event.preventDefault(); // Prevent the default anchor behavior
-            for (var i = maxVisibleEvents; i < allEvents.length; i++) {
-                allEvents[i].style.display = "none";
-            }
-            showEventsButton.style.display = "block";
-            hideEventsButton.style.display = "none";
-        });
-    }
-});
+//     if (eventList && showEventsButton && hideEventsButton) {
+//         var allEvents = eventList.querySelectorAll("li");
+//         var maxVisibleEvents = 4;
+
+//         // Hide all events initially
+//         for (var i = maxVisibleEvents; i < allEvents.length; i++) {
+//             allEvents[i].style.display = "none";
+//         }
+
+//         // Show more or hide events on button click
+//         showEventsButton.addEventListener("click", function (event) {
+//             event.preventDefault(); // Prevent the default anchor behavior
+//             for (var i = maxVisibleEvents; i < allEvents.length; i++) {
+//                 allEvents[i].style.display = "block";
+//             }
+//             showEventsButton.style.display = "none";
+//             hideEventsButton.style.display = "block";
+//         });
+
+//         hideEventsButton.addEventListener("click", function (event) {
+//             event.preventDefault(); // Prevent the default anchor behavior
+//             for (var i = maxVisibleEvents; i < allEvents.length; i++) {
+//                 allEvents[i].style.display = "none";
+//             }
+//             showEventsButton.style.display = "block";
+//             hideEventsButton.style.display = "none";
+//         });
+//     }
+// });
