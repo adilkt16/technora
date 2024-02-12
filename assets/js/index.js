c8e68475683c483c9526a432192c2be586ba846b
adilkt <adilkt16@gmail.com>
2024-02-13 00:48:47 +0530

    contents changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,38 +6,18 @@ function toggleMenu() {
     burgerMenu.classList.toggle('cross');
 }
 
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     var eventList = document.getElementById("event-list");
-//     var showEventsButton = document.getElementById("showEvents");
-//     var hideEventsButton = document.getElementById("hideEvents");
-
-//     if (eventList && showEventsButton && hideEventsButton) {
-//         var allEvents = eventList.querySelectorAll("li");
-//         var maxVisibleEvents = 4;
-
-//         // Hide all events initially
-//         for (var i = maxVisibleEvents; i < allEvents.length; i++) {
-//             allEvents[i].style.display = "none";
-//         }
-
-//         // Show more or hide events on button click
-//         showEventsButton.addEventListener("click", function (event) {
-//             event.preventDefault(); // Prevent the default anchor behavior
-//             for (var i = maxVisibleEvents; i < allEvents.length; i++) {
-//                 allEvents[i].style.display = "block";
-//             }
-//             showEventsButton.style.display = "none";
-//             hideEventsButton.style.display = "block";
-//         });
-
-//         hideEventsButton.addEventListener("click", function (event) {
-//             event.preventDefault(); // Prevent the default anchor behavior
-//             for (var i = maxVisibleEvents; i < allEvents.length; i++) {
-//                 allEvents[i].style.display = "none";
-//             }
-//             showEventsButton.style.display = "block";
-//             hideEventsButton.style.display = "none";
-//         });
-//     }
-// });
+ScrollReveal({
+    reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+  });
+  ScrollReveal().reveal(
+    ".home-content, .heading, .skills-heading,.login-heading",
+    { origin: "top" }
+  );
+  ScrollReveal().reveal("div.main-image, .container img,.about-img,.skill-child:nth-child(2)", {
+    origin: "bottom",
+  });
+  ScrollReveal().reveal(".home-content h2", { origin: "left" });
+  ScrollReveal().reveal(".home-content h3", { origin: "right" });
